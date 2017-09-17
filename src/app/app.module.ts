0a09b3fdeec530e8f4e936c9085fa68b9f4ab6f9
Ricardo <rb.viller@gmail.com>
2017-09-17 17:58:54 -0300

    Getting Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,21 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { AboutPage } from '../pages/about/about';
+import { MenuPage } from '../pages/menu/menu';
+import { ContactPage } from '../pages/contact/contact';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    AboutPage,
+    MenuPage,
+    ContactPage
   ],
   imports: [
     BrowserModule,
@@ -23,12 +28,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    AboutPage,
+    MenuPage,
+    ContactPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
